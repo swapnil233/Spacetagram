21c5c4946043f4d1903428bb0134c70f96750914
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-15 00:37:02 -0500

    Rendering data using ejs

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const app = express();
 
+const axios = require("axios");
+
 // Environment Variables
-require('dotenv').config;
-const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv")
+dotenv.config();
+const PORT = process.env.PORT || 3000
+const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&start_date=2022-01-01`
 
 // EJS View Engine
+app.set("views", "views");
 app.set("view engine", "ejs");
-
-// Listen
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
 
 // Serve static files
 app.use(express.static('public'));
+
+app.get("/", async (req, res) => {
+    const result = await axios.get(url);
+    const data = result.data;
+
+    res.render("index", {
+        data
+    })
+})
+
+// Listen
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log("Server error!")
+    }
+
+    console.log(`App listening on port ${PORT}`)
+})
